1b5748939bdeb2e3eb5c2066b5417418c7ba60e0
Priya Malewadkar <malewadkarpriya@gmail.com>
2026-06-23 02:43:08 +0530

    feat(terraform): initialize project with provider, security group and dynamic AMI lookup

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ssm_parameter" "amazon_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "terraform-web-sg"
+
+
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_iam_role" "ec2_ssm_role" {
+
+  name = "ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
