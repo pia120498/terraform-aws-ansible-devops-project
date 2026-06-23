f47decb40ffd8f3ec22618887917178efd2ac3bc
Priya Malewadkar <malewadkarpriya@gmail.com>
2026-06-23 15:42:48 +0530

    feat(iam): attach AmazonSSMManagedInstanceCore policy to EC2 role

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,3 +50,11 @@ resource "aws_iam_role" "ec2_ssm_role" {
     ]
   })
 }
+
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+
+  role = aws_iam_role.ec2_ssm_role.name
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
