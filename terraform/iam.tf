d03c055b6f28e4b7bc062705d4bc474e94e4c9dc
Priya Malewadkar <malewadkarpriya@gmail.com>
2026-06-24 13:34:17 +0530

    feat(cloudwatch): attach CloudWatch Agent policy to EC2 IAM role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -28,6 +28,12 @@ resource "aws_iam_role_policy_attachment" "ssm_policy" {
 }
 
 
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent_policy" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+
 resource "aws_iam_instance_profile" "ec2_profile" {
 
   name = "ec2-ssm-profile"
